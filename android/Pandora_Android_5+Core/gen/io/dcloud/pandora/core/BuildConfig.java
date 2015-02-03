9aab850f84d3dca1ffd7b7ddbaea9a5146f47b23
lleegu <leegu1987@gmail.com>
2015-02-03 15:58:12 +0800

    提交插件开发环境基础版本

---
+++ android/Pandora_Android_5+Core/gen/io/dcloud/pandora/core/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package io.dcloud.pandora.core;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/Pandora_Android_5+Core/src/io/dcloud/RInformation.java
@@ -0,0 +1,52 @@
+package io.dcloud;
+
+import io.dcloud.pandora.core.R;
+/**
+ * 此类文件中不能使用final定义变量
+ *
+ * @version 1.0
+ * @author yanglei Email:yanglei@d-heaven.com
+ * @Date 2013-7-1 下午12:17:58 created.
+ * 
+ * <br/>
+ *       Create By: yanglei Email:yanglei@d-heaven.com at 2013-7-1 下午12:17:58
+ */
+public class RInformation {
+	/**
+	 * drawable
+	 */
+	private static int DRAWABLE_SPLASH = R.drawable.splash;
+	private static int DRAWABLE_ICON = R.drawable.icon;
+	// ------------Map_RIn----end
+
+	public static int LAYOUT_PROGRESSBAR = R.layout.snow_white_progress;
+	// ------------Nav_RIn----begin
+	public static int DRAWABLE_NAVBG = R.drawable.navbg;
+	public static int DRAWABLE_TITLEBG = R.drawable.titlebg;
+	public static int DRAWABLE_PRESSBG = R.drawable.pressbg;
+	public static int DRAWABLE_SEARCH = R.drawable.search;
+	public static int DRAWABLE_LINE = R.drawable.line;
+	// ------------Nav_RIn----end
+
+	// /Action Sheet Start
+	public static int[] ACTS_STYLE_ACTIONSHEET = R.styleable.ActionSheet;
+	public static int ACTS_STYLE_actionSheetBackground = R.styleable.ActionSheet_actionSheetBackground;
+	public static int ACTS_STYLE_cancelButtonBackground = R.styleable.ActionSheet_cancelButtonBackground;
+	public static int ACTS_STYLE_otherButtonTopBackground = R.styleable.ActionSheet_otherButtonTopBackground;
+	public static int ACTS_STYLE_otherButtonMiddleBackground = R.styleable.ActionSheet_otherButtonMiddleBackground;
+	public static int ACTS_STYLE_otherButtonBottomBackground = R.styleable.ActionSheet_otherButtonBottomBackground;
+	public static int ACTS_STYLE_ActionSheet_cancelButtonTextColor = R.styleable.ActionSheet_otherButtonSingleBackground;
+	public static int ACTS_STYLE_cancelButtonTextColor = R.styleable.ActionSheet_cancelButtonTextColor;
+	public static int ACTS_STYLE_otherButtonTextColor = R.styleable.ActionSheet_otherButtonTextColor;
+	public static int ACTS_STYLE_actionSheetPadding = R.styleable.ActionSheet_actionSheetPadding;
+	public static int ACTS_STYLE_otherButtonSpacing = R.styleable.ActionSheet_otherButtonSpacing;
+	public static int ACTS_STYLE_cancelButtonMarginTop = R.styleable.ActionSheet_cancelButtonMarginTop;
+	public static int ACTS_STYLE_actionSheetTextSize = R.styleable.ActionSheet_actionSheetTextSize;
+	public static int ACTS_STYLE_otherButtonSingleBackground = R.styleable.ActionSheet_otherButtonSingleBackground;
+	public static int ACTS_STYLE_ActionSheetStyleIOS7 = R.style.ActionSheetStyleIOS7;
+	public static int ACTS_STYLE_otherButtonTitleBackground = R.styleable.ActionSheet_otherButtonTitleBackground;
+	public static int ACTS_STYLE_titleButtonTextColor = R.styleable.ActionSheet_titleButtonTextColor;
+	public static int ACTS_STYLE_destructiveButtonTextColor = R.styleable.ActionSheet_destructiveButtonTextColor;
+	public static int ACTS_ATTR_SctionSheetSytle = R.attr.actionSheetStyle;
+	// /Action Sheet End
+}
